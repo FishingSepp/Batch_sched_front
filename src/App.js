a982c29e53e8896e74734ae3a6689bf4f118db69
FishingSepp <lukas.krombholz@web.de>
2023-03-11 11:23:36 +0100

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,238 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from "react";
 import './App.css';
+import createIcon from './create-icon.png';
+import refreshIcon from './refresh-icon.png';
+import editIcon from './edit-icon.png';
+import deleteIcon from './delete-icon.png';
+import Modal from "react-modal";
+import CreateJobModal from "./CreateJobModal";
+import EditJobModal from "./EditJobModal";
+import axios from "axios";
+
+
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/jobs")
+      .then((response) => {
+        setJobs(response.data);
+        console.log(response.data); // log the jobs array
+      })
+      .catch((error) => {
+        console.error("Error fetching jobs:", error);
+      });
+  }, []);
+
+  const MAX_JOBS_DISPLAYED = 10; // maximum number of jobs to display
+
+  const [searchTermName, setSearchTermName] = useState("");
+  const [searchTermId, setSearchTermId] = useState("");
+  const [statusFilter, setStatusFilter] = useState("All");
+  const [displayedJobs, setDisplayedJobs] = useState([]);
+  const [createIsOpen, setCreateIsOpen] = useState(false);
+  const [editIsOpen, setEditIsOpen] = useState(false);
+
+  function handleStatusFilter(event) {
+    const value = event.target.value;
+
+    if (value === "All") {
+      setStatusFilter(["All"]);
+    } else {
+      const index = statusFilter.indexOf(value);
+      if (index === -1) {
+        setStatusFilter([...statusFilter, value]);
+      } else {
+        setStatusFilter(statusFilter.filter((item) => item !== value));
+      }
+    }
+  }
+
+  const handleSearchName = (event) => {
+    const value = event.target.value;
+    setSearchTermName(value);
+    setSearchTermId("");
+    setStatusFilter("All");
+    if (value === "") {
+      setDisplayedJobs([]);
+      return;
+    }
+    const filteredJobs = jobs.filter((job) =>
+      job.name.toLowerCase().startsWith(value.toLowerCase())
+    );
+    setDisplayedJobs(filteredJobs.slice(0, MAX_JOBS_DISPLAYED));
+  };
+  
+
+  const handleSearchId = (event) => {
+    const value = event.target.value;
+    setSearchTermId(value);
+    setSearchTermName("");
+    setStatusFilter("All");
+    if (value === "") {
+      setDisplayedJobs([]);
+    } else {
+      const filteredJobs = jobs.filter((job) => 
+        job.id.startsWith(value));
+      setDisplayedJobs(filteredJobs.slice(0, MAX_JOBS_DISPLAYED));
+    }
+  };  
+
+  const handleRefresh = () => {
+    // Add code to refresh the job list here
+  };
+
+  const handleCreate = () => {
+    // Add code to open the create job window here
+    setCreateIsOpen(true);
+  };
+
+  const closeCreate = () => {
+    setCreateIsOpen(false);
+  };
+
+  function handleEdit(id) {
+    // Logic for handling edit
+    setEditIsOpen(true);
+  }
+
+  const closeEdit = () => {
+    setEditIsOpen(false);
+  };
+  
+  function handleDelete(id) {
+    // Logic for handling delete
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Batch Scheduler</h1>
       </header>
+      <div className="App-container">
+        <div className="App-options">
+        <div className="btn-container">
+          <button className="create-btn" onClick={handleCreate}>
+          <img src={createIcon} alt="create icon" className="create-icon" />
+            <span>Create Job</span>
+          </button>
+          <button className="refresh-btn" onClick={handleRefresh}>
+          <img src={refreshIcon} alt="refresh icon" className="refresh-icon" />
+            <span>Refresh</span>
+          </button>
+        </div>
+       
+          <div className="App-search-container">
+            <div className="App-search">
+              <input
+                type="text"
+                placeholder="Search by name"
+                value={searchTermName}
+                onChange={handleSearchName}
+              />
+            </div>
+            <div className="App-search">
+              <input
+                type="text"
+                placeholder="Search by ID"
+                value={searchTermId}
+                onChange={handleSearchId}
+              />
+            </div>
+          </div>
+          <div className="App-status">
+            <label>Filter by status:</label>
+            <div className="status-filters">
+              <div className="status-filter">
+                <div className="status-label-container" onClick={() => setStatusFilter("All")}>
+                  <input type="checkbox" id="all" value="All" checked={statusFilter === "All"} onChange={() => {}} />
+                  <label htmlFor="all">All</label>
+                </div>
+              </div>
+              <div className="status-filter">
+                <div className="status-label-container" onClick={() => setStatusFilter("Running")}>
+                  <input type="checkbox" id="running" value="Running" checked={statusFilter === "Running"} onChange={() => {}} />
+                  <label htmlFor="running">Running</label>
+                </div>
+              </div>
+              <div className="status-filter">
+                <div className="status-label-container" onClick={() => setStatusFilter("Completed")}>
+                  <input type="checkbox" id="completed" value="Completed" checked={statusFilter === "Completed"} onChange={() => {}} />
+                  <label htmlFor="completed">Completed</label>
+                </div>
+              </div>
+              <div className="status-filter">
+                <div className="status-label-container" onClick={() => setStatusFilter("Failed")}>
+                  <input type="checkbox" id="failed" value="Failed" checked={statusFilter === "Failed"} onChange={() => {}} />
+                  <label htmlFor="failed">Failed</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="App-list">
+          <table>
+            <thead>
+              <tr className="table-headers">
+                <th>Select</th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Status</th>
+                <th>LastRun</th>
+                <th>NextRun</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody className="table-body">
+              {jobs
+                .filter((job) => {
+                  if (statusFilter === "All") {
+                    return true;
+                  } else {
+                    return job.status === statusFilter;
+                  }
+                })
+                .filter((job) => {
+                  if (searchTermName) {
+                    return job.name.toLowerCase().includes(searchTermName.toLowerCase());
+                  } else if (searchTermId) {
+                    return job.id.toString().includes(searchTermId);
+                  } else {
+                    return true;
+                  }
+                })
+                .map((job) => (
+                  <tr key={job.id}>
+                    <td><input type="checkbox" /></td>
+                    <td>{job.id}</td>
+                    <td>{job.name}</td>
+                    <td>{job.status}</td>
+                    <td>{job.lastRun}</td>           
+                    <td>{job.nextRun}</td>
+                    <td className="actions">
+                      <button className="edit-btn" onClick={() => handleEdit(job.id)}>
+                        <img src={editIcon} alt="edit icon" className="edit-icon" />
+                      </button>
+                      <button className="delete-btn" onClick={() => handleDelete(job.id)}>
+                        <img src={deleteIcon} alt="delete icon" className="delete-icon" />
+                      </button>
+                    </td>
+                  </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <CreateJobModal isOpen={createIsOpen} closeModal={closeCreate} />
+        <EditJobModal isOpen={editIsOpen} closeModal={closeEdit} />
+
+      </div>
+      <div className="App-footer">
+        <p>Batch Scheduler by Fishi</p>
+      </div>
     </div>
   );
 }
---
+++ src/EditJobModal.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import './EditJobModal.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+
+const EditJobModal = ({isOpen, closeModal}) => {
+    const [jobName, setJobName] = useState("");
+    const [jobDescription, setJobDescription] = useState("");
+    const [periodBegin, setPeriodBegin] = useState(null);
+    const [periodEnd, setPeriodEnd] = useState(null);
+    const [repeatInterval, setRepeatInterval] = useState(0);
+    const [repeatUnit, setRepeatUnit] = useState("minute(s)");
+    const [enabled, setEnabled] = useState(true);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // TODO: Handle form submission
+    };
+
+    const summary = () => {
+      let summaryText = `Start on ${
+        periodBegin ? periodBegin.toLocaleDateString() : ""
+      }`;
+      if(periodBegin){
+        if (repeatInterval > 0) {
+          if (periodEnd) {
+            summaryText += ` until ${periodEnd.toLocaleDateString()}`;
+          }
+          summaryText += ` and repeat every ${repeatInterval} ${repeatUnit}`;
+        } else {
+          summaryText += ` and execute once`;
+        }
+      }
+      if(!periodBegin){
+        summaryText = "Please select a Period Begin."
+      }
+    
+      
+    
+      return summaryText;
+    }
+    
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={closeModal}
+            contentLabel="Edit Job Modal"
+            style={{
+                overlay: {
+                    backgroundColor: 'rgba(44, 49, 58, 0.75)'
+                },
+                content: {
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '70%',
+                    width: '70%',
+                    backgroundColor: '#2c313a',
+                    color: 'white',
+                    padding: '20px',
+                    borderRadius: '10px',
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.25)'
+                }
+            }}>
+            <h1>Edit Job</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <div className="general-job-info">
+                        <div className="input-row">
+                            <label htmlFor="jobName">Name:</label>
+                            <br/>
+                            <input
+                                type="text"
+                                id="jobName"
+                                value={jobName}
+                                onChange={(e) => setJobName(e.target.value)}/>
+                        </div>
+                        <div className="input-row">
+                            <label htmlFor="jobDescription">Description:</label>
+                            <br/>
+                            <textarea id="jobDescription" value={jobDescription} onChange={(e) => setJobDescription(e.target.value)} rows="5"
+                                // Number of visible lines
+                            />
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="detail-job-info">
+                    <div className="date-picker-container">
+                        <label htmlFor="periodBegin">Period Begin:</label>
+                        <DatePicker
+                            selected={periodBegin}
+                            onChange={(date) => setPeriodBegin(date)}
+                            showTimeSelect="showTimeSelect"
+                            dateFormat="MMMM d, yyyy h:mm aa"/>
+                        <br/>
+                        <label htmlFor="periodEnd">Period End:</label>
+                        <DatePicker
+                            selected={periodEnd}
+                            onChange={(date) => setPeriodEnd(date)}
+                            showTimeSelect="showTimeSelect"
+                            dateFormat="MMMM d, yyyy h:mm aa"/>
+                    </div>
+                    <div className="repeat-container">
+                        <label htmlFor="repeatInterval">Repeat every:</label>
+                        <input
+                            type="number"
+                            id="repeatInterval"
+                            value={repeatInterval}
+                            onChange={(e) => setRepeatInterval(e.target.value)}/>
+                        <select
+                            id="repeatUnit"
+                            value={repeatUnit}
+                            onChange={(e) => setRepeatUnit(e.target.value)}>
+                            <option value="minute(s)">Minute(s)</option>
+                            <option value="hours(s)">Hour(s)</option>
+                            <option value="day(s)">Day(s)</option>
+                            <option value="week(s)">Week(s)</option>
+                            <option value="month(s)">Month(s)</option>
+                            <option value="year(s)">Year(s)</option>
+                        </select>
+                        <div className="summary-container">
+                            {summary()}
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <div className="footer-button-container">
+                    <button type="submit"
+                        //code for creating job
+                    >Edit</button>
+                    <button type="button" onClick={closeModal}>
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </Modal>
+    );
+};
+
+export default EditJobModal;
